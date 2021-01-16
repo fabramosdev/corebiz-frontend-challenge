37097a1044c26e43703be977c1a43bc0c412c42e
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2021-01-16 11:41:09 -0300

    :sparkles: Api Corebiz connected
    conexão a api teste da corebiz

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
+import api from '../../services/api';
 
 import { Container } from './styles';
 
-const Products: React.FC = () => (
-  <Container>
-    <h1>Products</h1>
-  </Container>
-);
+interface IProductsProps {
+  productId: number;
+  productName: string;
+  stars: number;
+  imageUrl: string;
+  listPrice: number | null;
+  price: number;
+  installments: [{
+    quantity: number;
+    value: number;
+  }];
+}
+
+const Products:React.FC = () => {
+  const [listProducts, setListProducts] = useState<IProductsProps[]>([]);
+
+  const getProducts = useCallback(async () => {
+    try {
+      await api.get('/products').then((products) => setListProducts(products.data));
+    } catch (err) {
+      alert(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  console.log(listProducts);
+
+  return (
+    <Container>
+      <h3>Mais Vendidos</h3>
+
+      {listProducts.map((item) => (
+        <>
+          <div className="product-item">
+            <img src={item.imageUrl} alt="Imagem" />
+            <div className="info">
+              <span>{item.productName}</span>
+              <span>{item.stars}</span>
+              <span>{item.listPrice ? `de ${item.listPrice}` : ''}</span>
+              <span>{`por ${item.price}`}</span>
+              <span>
+                {item.installments.length > 0 ? `ou em ${item.installments[0].quantity} de ${item.installments[0].value}` : ''}
+              </span>
+              <button type="button">COMPRAR</button>
+            </div>
+          </div>
+        </>
+      ))}
+
+    </Container>
+  );
+};
 
 export default Products;
--- src/components/Products/styles.ts
+++ src/components/Products/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  max-width: 1200px;
 
+  .product-item {
+    display: flex;
+    flex-direction: column;
+
+    .info{
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+
+      button {
+        display: none;
+      }
+  }
+    }
+
+
+  .product-item:hover {
+    .info {
+      background: yellow;
+
+      button {
+      display: block;
+    }
+    }
+
+
+  }
 `;
