35a30728fd93fe9e302cbd02255c2db5f424f45a
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2021-01-20 22:08:40 -0300

    :sparkles: Footer OK
    Footer FInalizado

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
+import btn1 from '../../assets/btn_contact-1.png';
+import btn2 from '../../assets/btn_contact-2.png';
+import logoCorebiz from '../../assets/corebiz.png';
+import logoVtex from '../../assets/logo-vtex.png';
 import { Container } from './styles';
 
 const Footer: React.FC = () => (
   <Container>
-    <h1>Footer</h1>
+    <div className="main-container">
+      <div className="address">
+        <h3>Localização</h3>
+        <p>Avenida Andromeda, 200. Bloco 6 e 8</p>
+        <p>Alphaville SP</p>
+        <a href="mailto:teste@mail.com">teste@teste.com</a>
+        <a href="tel:+552199999999">21 99999-9999</a>
+      </div>
+      <div className="contact">
+
+        <img src={btn2} alt="Fale conosco" />
+
+        <img src={btn1} alt="Fale Conosco" />
+
+      </div>
+      <div className="brand">
+        <div className="corebiz">
+          <p>Created by</p>
+          <img src={logoCorebiz} alt="Core Biz" />
+        </div>
+        <div className="vtex">
+          <p>Powered by</p>
+          <img src={logoVtex} alt="VTEX" />
+        </div>
+      </div>
+    </div>
+
   </Container>
 );
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,62 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  background: #000;
+  height: 225px;
+  width: 100%;
+  color: white;
 
+  .main-container {
+    display: flex;
+    max-width: 1200px;
+    justify-content: space-between;
+    margin: 0 auto;
+  }
+
+  .address {
+    padding-top: 25px;
+    width: 33.33%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .address h3 {
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  .address h3::after {
+    display: block;
+    height: 5px;
+    width: 45px;
+    background-color: white;
+    content: '';
+    margin: 10px 0;
+    border-radius: 5px;
+}
+
+.address a {
+  text-decoration: none;
+  color: white;
+}
+
+  .contact {
+    display: flex;
+    flex-direction: column;
+    padding: 40px;
+
+    img {
+      padding: 10px;
+    }
+  }
+  .brand {
+    width: 33.33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .corebiz {
+      padding-right: 15px;
+    }
+  }
 `;
